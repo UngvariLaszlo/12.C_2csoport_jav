0e3b95fbc709071be4f398c9fe2307eed4b19607
UngvariLaci <ungvari.laszlo06@gmail.com>
2024-12-04 10:34:30 +0100

    Add project files.

---
+++ KorHenger_UnitTest/Kör_teszt.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Kor_es_henger;
+
+namespace KorHenger_UnitTest
+{
+    [TestClass]
+    public class Kör_teszt
+    {
+        [TestMethod]
+        public void Kerület_teszt()
+        {
+            //Arange - Feltételek beállítása
+            double sugár = 5;
+            Kör kör = new Kör(sugár);
+            double vártEredm = 31.42;
+
+            //Act - Végrehajtás
+            kör.SetKerület();
+            double kapottEredm = kör.GetKerület();
+
+            //Assert kiértékelés
+            Assert.AreEqual(vártEredm, kapottEredm);
+        }
+
+        [TestMethod]
+        public void Terület_teszt()
+        {
+
+        }
+    }
+}
---
+++ KorHenger_UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KorHenger_UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KorHenger_UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("06dcbb88-e218-4608-97e7-ae565edf46aa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
